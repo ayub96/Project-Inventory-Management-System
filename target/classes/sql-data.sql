95201d4bc66dc9cae9516b50dd54c0bec38e1bf3
ayub <ayusuf@academytrainee.com>
2021-01-02 18:05:45 +0000

    removed target from gitignore

---
+++ target/classes/sql-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('peter', 'smith');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jake', 'paul');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('john', 'smith');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('michael', 'peterson');
+
+INSERT INTO `ims`.`items` (`name`, `quantity`, `price`) VALUES ('GTA', 54, 45.99);
+INSERT INTO `ims`.`items` (`name`, `quantity`, `price`) VALUES ('PUBG', 74, 55.99);
+INSERT INTO `ims`.`items` (`name`, `quantity`, `price`) VALUES ('COD', 541, 49.99);
+INSERT INTO `ims`.`items` (`name`, `quantity`, `price`) VALUES ('GTA5', 67, 59.99);
+INSERT INTO `ims`.`items` (`name`, `quantity`, `price`) VALUES ('Dark Souls', 32, 49.99);
+INSERT INTO `ims`.`items` (`name`, `quantity`, `price`) VALUES ('PUBG', 98, 29.99);
